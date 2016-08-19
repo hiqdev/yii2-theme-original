e7b042e78fcdca18e6746e59e258bb374cc41230
Andrii Vasyliev <sol@hiqdev.com>
2016-08-19 13:34:06 +0000

    redone menu with MenuManager

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -33,25 +33,9 @@ Yii::$app->get('themeManager')->registerAssets();
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
-    echo Nav::widget([
+    echo Yii::$app->get('menuManager')->render('main', [
+        'class'   => Nav::class,
         'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
     ]);
     NavBar::end();
     ?>
