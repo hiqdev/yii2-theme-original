657cf0f6302d15aa206eb1d3313ee1536dd5acab
Andrii Vasyliev <sol@hiqdev.com>
2016-08-22 10:25:16 +0000

    added org name and app name substitution

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -27,7 +27,7 @@ Yii::$app->get('themeManager')->registerAssets();
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -50,7 +50,7 @@ Yii::$app->get('themeManager')->registerAssets();
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; <?= date('Y') ?> <?= Yii::$app->params['orgName'] ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
