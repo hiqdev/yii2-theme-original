6f99ecb7661a398a0c0754a41f1bf6b3b7fe7240
Andrii Vasyliev <sol@hiqdev.com>
2016-09-01 18:06:54 +0000

    used PoweredBy widget instead of _poweredBy

--- src/views/layouts/_footer.php
+++ src/views/layouts/_footer.php
@@ -3,6 +3,9 @@
         <p class="pull-left">
             &copy; <?= Yii::$app->themeManager->widget('CopyrightYears') ?> <?= Yii::$app->themeManager->widget('OrganizationLink') ?>.
             <?= Yii::t('hiqdev/themes/original', 'All rights reserved.') ?>
-        <p class="pull-right"><?= $this->render('//layouts/_poweredBy') ?></p>
+        </p>
+        <p class="pull-right">
+            <?= Yii::$app->themeManager->widget('PoweredBy') ?>
+        </p>
     </div>
 </footer>
