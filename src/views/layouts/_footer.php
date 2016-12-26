ec0be1769b0d37f4e3c128e644580eb53785865a
tofid <andreyklochok@gmail.com>
2016-12-26 18:05:59 +0200

    Changed translation dictionary

--- src/views/layouts/_footer.php
+++ src/views/layouts/_footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <p class="pull-left">
             &copy; <?= \hiqdev\thememanager\widgets\CopyrightYears::widget() ?> <?= \hiqdev\thememanager\widgets\OrganizationLink::widget()?>.
-            <?= Yii::t('hiqdev/themes/original', 'All rights reserved.') ?>
+            <?= Yii::t('hiqdev:themes:original', 'All rights reserved.') ?>
         </p>
         <p class="pull-right">
             <?= \hiqdev\thememanager\widgets\PoweredBy::widget() ?>
