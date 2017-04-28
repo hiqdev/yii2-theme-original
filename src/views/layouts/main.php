5620064e1441cada95f251b2c5f1c28aef970333
tofid <andreyklochok@gmail.com>
2017-04-28 19:50:30 +0300

    Changed markup in main layout, changed css styles

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -6,11 +6,9 @@
     </head>
     <body>
     <?php $this->beginBody() ?>
-        <div class="wrap">
-            <?= $this->render('//layouts/_header') ?>
-            <?= $this->render('//layouts/_after_header') ?>
-            <?= $this->render('//layouts/_content', compact('content')) ?>
-        </div>
+        <?= $this->render('//layouts/_header') ?>
+        <?= $this->render('//layouts/_after_header') ?>
+        <?= $this->render('//layouts/_content', compact('content')) ?>
 
         <?= $this->render('//layouts/_footer') ?>
         <?= $this->render('//layouts/_after_footer') ?>
